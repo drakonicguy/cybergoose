93b866750765ad89d75c6e68c1c0ef9d26f307f7
rollerupaint <34401597+rollerupaint@users.noreply.github.com>
2021-07-22 18:54:03 +0200

    Add files via upload

--- scripts/Head.gd
+++ scripts/Head.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+export (PackedScene) var laser_scene
 # warning-ignore:unused_argument
 #global_rotation_degrees << if we need to retrieve rotation degrees use this
 func _process(delta):
@@ -8,3 +9,11 @@ func _process(delta):
 		$Sprite.flip_v = true
 	else:
 		$Sprite.flip_v = false
+
+# warning-ignore:unused_argument
+func _input(event):
+	if Input.is_action_just_pressed("shoot"):
+		print("pew pew")
+		var laser = laser_scene.instance()
+		add_child(laser)
+		laser.look_at(get_global_mouse_position())
