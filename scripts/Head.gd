7868eecb97889ca5b34b6333a9a9eb69b5027126
souupp <87827244+souupp@users.noreply.github.com>
2021-07-23 19:23:01 +0000

    test
    second attempt at uploading the new files?

--- scripts/Head.gd
+++ scripts/Head.gd
@@ -1,19 +1,46 @@
 extends Node2D
 
+const MAX_SPEED = 800
+const ACCEL = 2000
+const DECEL = 500
+var velocity = 0
+var dir = 0
+var speed = 0
+
 export (PackedScene) var laser_scene
-# warning-ignore:unused_argument
+
 #global_rotation_degrees << if we need to retrieve rotation degrees use this
+
 func _process(delta):
+	
 	look_at(get_global_mouse_position())
 	if get_global_mouse_position().x < position.x:
 		$Sprite.flip_v = true
 	else:
 		$Sprite.flip_v = false
+	
+	_move(delta)
 
-# warning-ignore:unused_argument
-func _input(event):
+	pass
+
+func _input(_event):
 	if Input.is_action_just_pressed("shoot"):
-		print("pew pew")
 		var laser = laser_scene.instance()
-		add_child(laser)
-		laser.look_at(get_global_mouse_position())
+		owner.add_child(laser)
+		laser.transform = global_transform
+
+func _move(delta):
+	dir = 0
+	if Input.is_action_pressed("move_left"):
+		dir = -1
+	if Input.is_action_pressed("move_right"):
+		dir = 1
+	
+	if dir:
+		speed += ACCEL * delta
+	else:
+		speed -= DECEL * delta
+	speed = clamp(speed, 0, MAX_SPEED)
+	
+	velocity = speed * delta * dir
+	self.translate(Vector2(velocity, 0)) # changing node's position
