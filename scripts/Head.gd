7f93fd47010a3d66fa0e71c2c189afc583831d93
rollerupaint <34401597+rollerupaint@users.noreply.github.com>
2021-07-26 18:57:01 +0200

    HEAD WORKS PROPERLY !

--- scripts/Head.gd
+++ scripts/Head.gd
@@ -9,43 +9,39 @@ var speed = 0
 
 export (PackedScene) var laser_scene
 
+signal flip
+signal unflip
+
 func _ready():
-	pass
-#	below is a workaround for the center pivot point.
-#	$Sprite.position.y = 75
-#	$Sprite.animation = "default"
+	while true:
+		yield(self, "flip")
+		$Sprite.rotation_degrees +=180
+		yield(self, "unflip")
+		$Sprite.rotation_degrees -=180
 	
 
 func _process(_delta):
 	pass
-#	below is a workaround for the center pivot point.
-#	var distance2mouse = self.global_position.distance_to(get_global_mouse_position())
-#	look_at(get_global_mouse_position())
-#	if get_global_mouse_position().x < position.x:
-#		$Sprite.flip_v = true
-#		$Sprite.position.x = -57
-#		self.global_rotation_degrees = min(self.global_rotation_degrees, 30)
-#		if self.global_rotation_degrees == 30:
-#			$Sprite.flip_v = false
-#			$Sprite.flip_h = true
-#			self.global_rotation_degrees = -30
-#		else:
-#			$Sprite.flip_h = false
-#	else:
-#		$Sprite.flip_v = false
-#		$Sprite.flip_h = false
-#		$Sprite.position.x = -57
-#		self.global_rotation_degrees = min(self.global_rotation_degrees, 30)
-#	if distance2mouse <= 150:
-#		$Sprite.animation = "close"
-#	else:
-#		$Sprite.animation = "default"
+	var distance2mouse = self.global_position.distance_to(get_global_mouse_position())
+	look_at(get_global_mouse_position())
+	if get_global_mouse_position().x < position.x:
+		emit_signal("flip")
+		if distance2mouse <= 150:
+			$Sprite.animation = "closeL"
+		else:
+			$Sprite.animation = "defaultL"
+	else:
+		emit_signal("unflip")
+		if distance2mouse <= 150:
+			$Sprite.animation = "close"
+		else:
+			$Sprite.animation = "default"
 	
 func _input(_event):
 	if Input.is_action_just_pressed("shoot"):
 		var laser = laser_scene.instance()
 		add_child(laser)
-		if $Sprite.flip_v == true:
+		if get_global_mouse_position().x < position.x:
 			laser.position = $ShootPositionFlipped.position
 		else:
 			laser.position = $ShootPosition.position
